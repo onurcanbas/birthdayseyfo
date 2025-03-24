aa24d24e2d8cf49c9257bb2425b6c6f0951fd066
Onur Can Baş <61903833+onurcanbas@users.noreply.github.com>
2025-03-24 14:41:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ document.addEventListener('DOMContentLoaded', function() {
     const iframe = document.getElementById('birthdayVideo');
     
     // Google Drive video linki
-    const videoId = '1zVlo5JQqzEuB-TUf5D2cuawCAYDqlOT5';
+    const videoId = '10ANnKCj77DJD1bb7vZmWmog_Jx2Wwers';
     iframe.src = `https://drive.google.com/file/d/${videoId}/preview`;
 }); 
